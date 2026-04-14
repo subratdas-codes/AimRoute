1c850b663ae6e753a9820458a9359d719fd2b827
Student Subrat <subratdas219@gmail.com>
2026-04-14 21:57:11 +0530

    connect frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://aimroute.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
